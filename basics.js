9a7b5ed09ba2682f479d5bc1bf31c37695f9551b
getstack <muhammadwaqas.me@gmail.com>
2021-11-29 00:07:08 +0500

    more code added

--- basics.js
+++ basics.js
@@ -1,11 +1,60 @@
 // Declaration of varibales 
 
 // Declaring a let variable 
-let firstname = 'adam';
-    firstname= 'alex';
+let myname = 'adam';
+    myname= 'alex';
 
-console.log(firstname);
+//console.log(firstname);
 
 // Decalring a constant
 
+const lastName = 'Mosh';
+
+console.log (lastName);
+
+// Primitive Values
+
+let name = 'adam';// String 
+let age = 30 // Number 
+let isapproved = true; // Boolean
+let firstname = undefined ; 
+let selectedColor = null;
+
+// Reference Types 
+
+// Object 
+
+let person =  {
+
+    name :'Alien',
+    age : 30,
+
+}
  
+
+// Dot Notation 
+person.name = 'Elan'
+person.age  = 20;
+console.log (person);
+
+
+// Arrays 
+
+let selectedColors = ['red', 'green'];
+
+selectedColors.reverse = [1];
+
+console.log (selectedColors);
+
+
+// functions 
+
+function calculate (a , b) {
+
+return a * b ;
+
+}
+
+console.log(calculate (2,3));
+
+
