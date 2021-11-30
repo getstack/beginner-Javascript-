f9190527ce9704149cd955c90c3b98d9a8711254
getstack <muhammadwaqas.me@gmail.com>
2021-12-01 02:18:59 +0500

    ternary operator added

--- operator.js
+++ operator.js
@@ -20,12 +20,32 @@ console.log (x)
 
 let y = 1 ;
 
-console.log (y > 0);
-console.log (y >= 1);
-console.log (y < 1);
+//console.log (y > 0);
+//console.log (y >= 1);
+//console.log (y < 1);
 
 // Strict Equality  types and value 
 
-console.log (y === 1);
-console.log (y !== 1);
+console.log (y === 1); // true 
+console.log (y !== 1); // false
+console.log ('1' === 1);
+
+// Lose Equality Operators 
+
+console.log ('1' == 1);
+console.log (true == 1 )
+
+// ternary operators 
+
+let points = 110;
+let type = points > 100 ? 'Silver' : 'Gold';
+
+let marks = 90 ;
+
+let grade = marks > 100  ? 'A': 'B';
+
+console.log (grade)
+
+
+
 
