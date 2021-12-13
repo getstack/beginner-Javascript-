528e8969671bcc602a26ee3b1a3907b9a87d2cbe
getstack <muhammadwaqas.me@gmail.com>
2021-12-14 00:00:44 +0500

    arrays methods added

--- arrays.js
+++ arrays.js
@@ -11,14 +11,17 @@ colors.unshift (1,2);
 // End of 
 colors.splice (1,0,'a','b');
 
+// Delete the last element of array
+colors.pop ('pink');
+
 // Find the primitives values  in javacscript 
 
-const numbers = [1,2,3,4];
+// const numbers = [1,2,3,4];
 
 
-console.log(numbers.indexOf(3));
-console.log(numbers.lastIndexOf(1));
-console.log(numbers.includes(4));
+// console.log(numbers.indexOf(3));
+// console.log(numbers.lastIndexOf(1));
+// console.log(numbers.includes(4));
 
 // find the values of Reference type 
 const courses = [
@@ -48,5 +51,117 @@ const subject = subjects.find( subject => subject.name == 'x');
 
 //console.log (subject);
 
-// Arrows Function 
+// Removing Elements 
 
+let removeElements = [1,2,3,4,5,6];
+
+removeElements.splice (0,6); // Remove the elements from array ( index,delete the element)
+
+
+//Solution 1 to empty an array
+removeElements.length = 0;
+
+// Solution 2  assign to new empty array the existing array
+
+
+removeElements = []; 
+console.log(removeElements);
+
+
+// Combinig Arrays 
+
+const first = [1,2,3,4]
+const second = [5,6,7,8,9]
+
+const combined = first.concat (second);
+
+console.log (combined);
+
+
+
+
+const one = [ {id :1} , {id :2}  ,{id :3}  ,{id :4}  ,{id :5} ];
+
+one[0].id = 3;
+console.log (one);
+
+const question= [1,2,3,4];
+
+question.push (33,22,'a','c');
+question.unshift (33,55,2);
+console.log(question);
+
+//Combining Arrays 
+const v1 = [1,2,3,4];
+const v2 = [3,4,5,6,7,8];
+
+//const combine = v1.concat(v2);
+
+// Now we are using Spread operator 
+
+const combine = [...v1 , ...v2]
+
+console.log (combine);
+
+
+// const obj1 = {
+
+// id : 1,
+// Name : 'adam',
+// age : 20,
+
+// }
+
+// const obj2 = {
+
+//     id : 3,
+//     Name : 'Martin',
+//     age : 30,
+    
+//     }
+    
+// iterate the array 
+
+const numbers = [1,2,3];
+
+const joined = numbers.join ('-');
+
+console.log (joined);
+
+
+
+// for (let number of numbers){
+
+//     console.log (number)
+
+// }
+
+// numbers.forEach((number,index) => console.log (index,number));
+
+
+// Spread operators 
+
+const state = {
+
+    name : 'jhon' ,
+
+}
+
+const newstate = {
+
+...state,
+id:3,
+
+}
+
+console.log (newstate);
+
+// joing Arrays 
+
+const firsts = [1,2,3,4,5,6,7]
+
+const seconds = [5,6,7];
+
+const add = [...first , ...second];
+
+console.log (add);
