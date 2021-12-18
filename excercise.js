68d091f59d5a29c058f62a5f1cdfcea0430809a0
getstack <muhammadwaqas.me@gmail.com>
2021-12-18 16:48:39 +0500

    add the code

--- excercise.js
+++ excercise.js
@@ -2,7 +2,7 @@
 const numbers = arrayFromRange  (1,20);
 console.log (numbers);
 
-function arrayFromRange (min,max){
+ function arrayFromRange (min,max){
    
     const output = [];
     for (let i = min ; i <= max ; i++)
@@ -13,4 +13,49 @@ function arrayFromRange (min,max){
 
 }
 
-// 
+// Hositing called a variable or function before its declatattion 
+
+//Varibale Hoisting
+
+console.log (x);
+var x = 3;
+
+
+// // function Hoisting  
+
+ (c);
+
+function c (){
+    console.log ('lets build');
+}
+
+
+// Block Scope 
+
+let a = 20;
+
+{
+
+    var a = 10 ;
+    const b = 12;
+    var c = 'waseem'
+
+    
+    
+ }
+
+// Clousers 
+
+function outer () {
+            
+        let a = 10;
+        function inner () {
+            console.log (a);
+
+        }
+    return inner ();  
+
+}
+
+outer();
+
